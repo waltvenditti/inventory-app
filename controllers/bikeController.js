14c1b35dd7028aab2e12f421ba1d0e58ea5ac75d
WaltVenditi <waltvenditti@gmail.com>
2022-04-26 10:36:39 -0400

    implemented basic view for index and bike list. Need to repopulate db due to error involving != vs !==

--- controllers/bikeController.js
+++ controllers/bikeController.js
@@ -1,13 +1,44 @@
 var Bike = require("../models/bike");
 
+var async = require("async");
+
 // Display home page
 exports.index = function (req, res) {
-  res.send("NOT IMPLEMENTED: main index");
+  res.render("index", {title: "Bike Shop Inventory"});
 };
 
 // Display list of all bikes
-exports.bike_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: bike list");
+exports.bike_list = function (req, res, next) {
+  async.parallel(
+    {
+      street_bikes: function (callback) {
+        Bike.find({ class: "Street" }, "bike manf price invCount")
+          .sort({ bike: 1 })
+          .exec(callback);
+      },
+      gravel_bikes: function (callback) {
+        Bike.find({ class: "Gravel" }, "bike manf price invCount")
+          .sort({ bike: 1 })
+          .exec(callback);
+      },
+      mountain_bikes: function (callback) {
+        Bike.find({ class: "Mountain" }, "bike manf price invCount")
+          .sort({ bike: 1 })
+          .exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) { 
+        return next(err); 
+      }
+      res.render("bike_list", {
+        title: "Bike Inventory",
+        street_bike_list: results.street_bikes,
+        gravel_bike_list: results.gravel_bikes,
+        mountain_bike_list: results.mountain_bikes,
+      })
+    }
+  );
 };
 
 // Display info page for a specific bike
