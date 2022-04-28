27f7bf96d5790b764163379b914e8d4860a16c75
WaltVenditi <waltvenditti@gmail.com>
2022-04-28 09:15:20 -0400

    implemented update bike functionality

--- controllers/bikeController.js
+++ controllers/bikeController.js
@@ -111,6 +111,7 @@ exports.bike_create_post = [
         res.render("bike_form", {
           title: "Add New Bike",
           bike: bike,
+          errors: errors.array(),
         });
       } else {
         bike.save(function (err) {
@@ -125,13 +126,76 @@ exports.bike_create_post = [
 
 // Display form for bike update GET
 exports.bike_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: bike update get");
+  Bike.findById(req.params.id, function (err, bike) {
+    if (err) { return next(err); }
+    if (bike==null) {
+      var err = new Error("Bike not found in database");
+      err.status = 404;
+      return next(err);
+    }
+    res.render("bike_form", {
+      title: `Update Bike: ${bike.manf} ${bike.bike}`,
+      bike: bike,
+    })
+  })
 };
 
 // Process bike update POST request
-exports.bike_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: bike update post");
-};
+exports.bike_update_post = [
+  // Validate and sanitize all fields
+  body("bikeName").trim().isLength({ min: 1 }).escape().withMessage("Bike must have a name."),
+  body("price").trim().isLength({ min: 1 }).escape().withMessage("Bike must have a price.").isNumeric().withMessage("Price can be numbers (with decimal points) only."),
+  body("year").optional({ checkFalsy: true }).trim().escape(),
+  body("manf").optional({ checkFalsy: true }).trim().escape(),
+  body("frame").optional({ checkFalsy: true }).trim().escape(),
+  body("wheels").optional({ checkFalsy: true }).trim().escape(),
+  body("crankset").optional({ checkFalsy: true }).trim().escape(),
+  body("drivetrain").optional({ checkFalsy: true }).trim().escape(),
+  body("brakes").optional({ checkFalsy: true }).trim().escape(),
+  body("tires").optional({ checkFalsy: true }).trim().escape(),
+  body("class").trim().escape(),
+  body("invCount").trim().escape().isNumeric(),
+  
+  // Process request after validation/sanitization
+  (req, res, next) => {
+    const errors = validationResult(req);
+    Service.find({ serviceType: "Bike" }).sort({ service: 1 }).exec(function (err, results) {
+      if (err) { return next(err); }
+      var bike = new Bike({
+        _id: req.params.id,
+        bike: req.body.bikeName,
+        price: req.body.price,
+        year: req.body.year,
+        manf: req.body.manf,
+        frame: req.body.frame,
+        wheels: req.body.wheels,
+        crankset: req.body.crankset,
+        drivetrain: req.body.drivetrain,
+        brakes: req.body.brakes,
+        tires: req.body.tires,
+        invCount: req.body.invCount,
+        class: req.body.class,
+        services: results,
+      })
+      if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized./validated data and error messages
+        res.render("bike_form", {
+          title: "Update Bike",
+          bike: bike,
+          errors: errors.array(),
+        });
+        
+      } else {
+        Bike.findByIdAndUpdate(req.params.id, bike, {}, function (err, thebike) {
+          if (err) { return next(err); }
+          // Successful so redirect to page of new bike
+          res.redirect(thebike.url);
+        })
+      }
+    });
+  },
+
+];
 
 // Display form for bike delete GET
 // Displays key info on the bike and asks you to confirm the delete by pressing a DELETE button
--- models/bike.js
+++ models/bike.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
@@ -13,19 +13,19 @@ var BikeSchema = new Schema (
     drivetrain: {type: String},
     brakes: {type: String},
     tires: {type: String},
-    class: {type: String, enum: ['Street', 'Gravel', 'Mountain']},
+    class: {type: String, enum: ["Street", "Gravel", "Mountain"]},
     price: {type: Number, required: true},
     invCount: {type: Number},
-    services: [{type: Schema.Types.ObjectId, ref: 'Service'}]
+    services: [{type: Schema.Types.ObjectId, ref: "Service"}]
   }
 )
 
 // Virtual for bike URL
 BikeSchema
-  .virtual('url')
+  .virtual("url")
   .get(function () {
-    return '/index/bike/' + this._id; 
+    return "/index/bike/" + this._id; 
   });
 
 // Export model
-module.exports = mongoose.model('Bike', BikeSchema);
+module.exports = mongoose.model("Bike", BikeSchema);
