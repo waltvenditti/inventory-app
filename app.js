9f21a5ffc39b8aa3e38a8386d3966ed04b3a897b
WaltVenditi <waltvenditti@gmail.com>
2022-04-29 09:48:43 -0400

    modified package.json and set up variable for app to get database URI from OS env if such an env exists

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // connect to mongodb
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://waltvenditti:inventory-app-db@cluster0.yanjk.mongodb.net/bikeshop_inventory?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://waltvenditti:inventory-app-db@cluster0.yanjk.mongodb.net/bikeshop_inventory?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
